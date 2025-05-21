de7ca213a4bdada46475928646bb317f93a9abde
rebarxd <142716718+rebarxd@users.noreply.github.com>
2025-05-21 18:31:50 +0700

    script can running, but need modified more

--- myscript.js
+++ myscript.js
@@ -1,15 +1,15 @@
 function getComputerChoice(){
     var computerChoice = Math.floor(Math.random() * 3)
     if (computerChoice == 0){
-        return "Kertas"
+        return 0;
     } else if (computerChoice == 1){
-        return "Gunting"
+        return 1;
     } else if (computerChoice == 2){
-        return "Batu"
+        return 2;
     }
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 function getHumanChoice(){
     var humanChoice = prompt("Pilih antara K = Kertas, G = Gunting, B = Batu")
@@ -22,22 +22,39 @@ function getHumanChoice(){
     }
 }
 
+// console.log(getHumanChoice());
+
 var humanScore = 0;
 var computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
-    if (humanChoice == 0 && computerChoice == 0){
-        console.log("Draw")
-    } else if(humanChoice == 0 && computerChoice == 1){
-        console.log("You lose, system win")
-        computerScore++
-    } else if (humanChoice == 0 && computerChoice == 2){
-        console.log("You Win, system lose")
-        humanScore++
+    if (humanChoice == computerChoice){
+        console.log("Draw");
+    } else if (humanChoice == 0 && computerChoice == 1){  // Kertas vs Gunting
+        console.log("You Win, system lose");
+        humanScore++;
+    } else if (humanChoice == 0 && computerChoice == 2){  // Kertas vs Batu
+        console.log("You Lose, system win");
+        computerScore++;
+    } else if (humanChoice == 1 && computerChoice == 0){  // Gunting vs Kertas
+        console.log("You Lose, system win");
+        computerScore++;
+    } else if (humanChoice == 1 && computerChoice == 2){  // Gunting vs Batu
+        console.log("You Win, system lose");
+        humanScore++;
+    } else if (humanChoice == 2 && computerChoice == 0){  // Batu vs Kertas
+        console.log("You Win, system lose");
+        humanScore++;
+    } else if (humanChoice == 2 && computerChoice == 1){  // Batu vs Gunting
+        console.log("You Lose, system win");
+        computerScore++;
     }
 }
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+console.log(`Human choice: ${["Kertas", "Gunting", "Batu"][humanSelection]}`);
+console.log(`Computer choice: ${computerSelection}`);
 
 playRound(humanSelection, computerSelection);
